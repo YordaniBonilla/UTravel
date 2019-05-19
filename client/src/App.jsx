fded3bddfc932041579de918299e980a9b7f48f4
YordaniBonilla <b.yordani93@gmail.com>
2019-05-18 19:52:50 -0500

    Rename BrowseRouter as Router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './Components/Home.jsx';
 import Register from './Components/Register.jsx';
 import Login from './Components/Login.jsx';
@@ -9,13 +9,13 @@ import Login from './Components/Login.jsx';
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
+      <Router>
           <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/register" component={Register} />>
             <Route path="/login"  component={Login} />
           </Switch>
-    </BrowserRouter>
+    </Router>
     )
   }
 }
