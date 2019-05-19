4845ae8da5253fc10b244ba2b90d1beaadf1f83e
YordaniBonilla <b.yordani93@gmail.com>
2019-05-18 21:31:29 -0500

    used shorter syntax for react fragment in header component

--- client/src/Components/HeaderComponent.jsx
+++ client/src/Components/HeaderComponent.jsx
@@ -1,4 +1,4 @@
-import React, {Component, Fragment} from 'react';
+import React, {Component} from 'react';
 import {NavLink} from 'react-router-dom';
 
 
@@ -10,7 +10,7 @@ export default class Header extends Component {
                 <NavLink to='/'><button className="button is-danger is-outlined">Home</button></NavLink>
                 <NavLink to='/register'><button className="button is-danger is-outlined">Register</button></NavLink>
                 <NavLink to='/login'><button className="button is-danger is-outlined">Login</button></NavLink>
-            </>
+            </> // this is the shorter syntax for react fragment it allows you to wrap more than one element without having to wrap them all in individual div tags 
         )
     }
 }
