2111c5bd83ccde1dfb1767f0cb5e3c3aa21e2b6b
IvanCarrillOSolache <carrillo.solache@gmail.com>
2019-06-03 21:12:30 -0500

    accessed coresponding props from new object container containing all props

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -9,7 +9,7 @@ export default class Home extends Component {
           <header>
             <HeaderComponent />
           </header>
-          //the first {} is to get into javascript and the {}2 one is our object
+          {/*//the first {} is to get into javascript and the {}2 one is our object*/}
           <HomeCard userInfo = {{phone: "253-341-4432", email: "b.yordani93@gmail.com", name:"Paul", country:"Cambodia", imageUrl: "https://singularityhub.com/wp-content/uploads/2018/06/autonomous-vehicles-cars-blurred-hong-kong-city-street-view-night-558632776.jpg"}}/>
       </div>
     );
--- client/src/Components/HomeCard.js
+++ client/src/Components/HomeCard.js
@@ -7,8 +7,8 @@ function HomeCard (props) {
     return ( 
         <div>
          <img src={props.userInfo.imageUrl}/>
-         //here inside the h3 tag to read javascript an expression container is needed
-         //to access the passed in props we need to access the props object with the key name of the passed in value
+         {/*here inside the h3 tag to read javascript an expression container is needed*/}
+         {/*to access the passed in props we need to access the props object with the key name of the passed in value*/}
          <h3>Country:{props.userInfo.country}</h3>
          <p>Phone: {props.userInfo.phone}</p>
          <p>Email: {props.userInfo.email}</p>
