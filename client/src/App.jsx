a04ed0fb08c3b11d73f61e5342b30dc528fed46b
IvanCarrillOSolache <carrillo.solache@gmail.com>
2019-06-04 11:54:10 -0500

    created get request in login component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,15 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./Components/Home.jsx";
 import Register from "./Components/Register.jsx";
-
+import Login from "./Components/Login.jsx";
 class App extends Component {
   render() {
     return (
       <Router>
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route path="/register" component={Register} />>
+          <Route path="/register" component={Register} />
+          <Route path="/login" component={Login}/>
         </Switch>
       </Router>
     );
