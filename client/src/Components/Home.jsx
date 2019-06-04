b40f2563e91635205d86fb6d99ce1920864c14d1
YordaniBonilla <b.yordani93@gmail.com>
2019-06-03 19:35:31 -0500

    created new card component

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -1,33 +1,15 @@
 import React, { Component } from "react";
 import HeaderComponent from "./HeaderComponent";
-import { MDBCol } from "mdbreact";
-import Img from 'react-image';
-import Image from 'react-image-resizer';
+import HomeItem from "../Components/HomeItem"
 
 export default class Home extends Component {
   render() {
     return (
       <div>
-      <div className="App">
-        <header>
-          <HeaderComponent />
-        </header>
-       
-      </div>
-      <div>
-      Were are you going?
-      <MDBCol md="6">
-      <input className="form-control" type="text" placeholder="Search" aria-label="Search" />
-      </MDBCol>
-      </div>
-      <div>
-        <Img
-         src="https://singularityhub.com/wp-content/uploads/2018/06/autonomous-vehicles-cars-blurred-hong-kong-city-street-view-night-558632776.jpg"
-         width={350}
-         height={300}
-
-         />
-      </div>
+          <header>
+            <HeaderComponent />
+          </header>
+          <HomeItem/>
       </div>
     );
   }
