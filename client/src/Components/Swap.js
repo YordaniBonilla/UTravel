6e9aedd9b516223b35e3293396a0cfe86e789eb3
YordaniBonilla <b.yordani93@gmail.com>
2019-06-05 15:13:52 -0500

    uninstall react-image-upload and removed elements

--- client/src/Components/Swap.js
+++ client/src/Components/Swap.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import ImageUploader from 'react-images-upload';
 
 class Swap extends Component {
     constructor(props) {
@@ -9,23 +8,13 @@ class Swap extends Component {
         };
     }
 
-    onDrop(picture) {
-        this.setState({
-            pictures: this.state.pictures.concat(picture),
-        });
-    }
+   
 
     render () {
       return (
         <div>
         //need to be able to upload an image to database
-        <ImageUploader
-                withIcon={true}
-                buttonText='Choose images'
-                onChange={this.onDrop}
-                imgExtension={['.jpg', '.gif', '.png', '.gif']}
-                maxFileSize={5242880}
-            />
+    
         <input/>
         //user location which is country
         //about
