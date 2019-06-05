5a2b0f3192a53a1a80321936eb291ac75009a5c8
YordaniBonilla <b.yordani93@gmail.com>
2019-06-05 12:53:37 -0500

    now passing correct property to child of homeCard Component

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -7,7 +7,7 @@ export default class Home extends Component {
   constructor() {
     super();
     this.state = {
-      data: undefined
+      data: []
     }
   }
   
@@ -15,9 +15,8 @@ export default class Home extends Component {
     axios.get('/home')
     .then(response => {
       this.setState({
-        data: response.data["0"]
+        data: response.data
       }) 
-       console.log("this is the state",this.state.data)
     })
     .catch(function(err) {
       console.log(err);
@@ -29,23 +28,24 @@ export default class Home extends Component {
 
   }
   render() {
-     /*console.log(this.state)
-     const { data } = this.state
-     data.map(obj => {
-        return (
-            <HomeCard question={data.userlocation} punchLine={data.userphone} />
-        )
-    })*/
+     const {data} = this.state
     return (
       <div>
           <header>
             <HeaderComponent />
           </header>
-          {/*//the first {} is to get into javascript and the {}2 one is our object*/}
-          <HomeCard userInfo = {{phone: "253-341-4432", email: "b.yordani93@gmail.com", name:"Paul", country:"Cambodia", imageUrl: "https://singularityhub.com/wp-content/uploads/2018/06/autonomous-vehicles-cars-blurred-hong-kong-city-street-view-night-558632776.jpg"}}/>
-          <HomeCard userInfo = {{phone: "253-341-4432", email: "b.yordani93@gmail.com", name:"Paul", country:"Cambodia", imageUrl: "https://cdn-3.expansion.mx/dims4/default/d1fe8e1/2147483647/strip/true/crop/1262x831+0+0/resize/800x527!/quality/90/?url=https%3A%2F%2Fcdn-3.expansion.mx%2F44%2F24%2Feabb1af844068c92301ac513d133%2Fistock-860594926.jpg"}}/>
-          <HomeCard userInfo = {{phone: "253-341-4432", email: "b.yordani93@gmail.com", name:"Paul", country:"Cambodia", imageUrl: "https://www.contentedtraveller.com/wp-content/uploads/2016/04/dubai-Top-9-Things-to-do-in-Old-Dubaig.jpg"}}/>
-          <HomeCard userInfo = {{phone: "253-341-4432", email: "b.yordani93@gmail.com", name:"Paul", country:"Cambodia", imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/Buddhist_monks_in_front_of_the_Angkor_Wat.jpg/600px-Buddhist_monks_in_front_of_the_Angkor_Wat.jpg"}}/>
+          { data ? 
+            <div>
+            {data.map(data => (
+              <HomeCard
+              key={data.id} 
+              data={data}
+              />
+              ))}
+            </div> :
+            <h1>No Data</h1>
+          }
+         
       </div>
     );
   }
--- client/src/Components/HomeCard.js
+++ client/src/Components/HomeCard.js
@@ -3,18 +3,11 @@ import Img from 'react-image';
 import Image from 'react-image-resizer';
 
 function HomeCard (props) {
-
+    console.log("this",props)
     return ( 
-        <div>
-         <img src={props.userInfo.imageUrl} height={300} width={350}/>
-         {/*here inside the h3 tag to read javascript an expression container is needed*/}
-         {/*to access the passed in props we need to access the props object with the key name of the passed in value*/}
-         <h3>Country:{props.userInfo.country}</h3>
-         <p>Phone: {props.userInfo.phone}</p>
-         <p>Email: {props.userInfo.email}</p>
-         <p>Name: {props.userInfo.name}</p>
-         <hr/>
-        </div>
+<div>
+<h4>{props.data.fname}</h4>
+</div>
         );
    
 }
