33df6b3c5be43add8b6899ded5e0759e6c403b72
YordaniBonilla <b.yordani93@gmail.com>
2019-06-05 15:24:05 -0500

    created file select handler function and file upload function

--- client/src/Components/Swap.js
+++ client/src/Components/Swap.js
@@ -1,20 +1,28 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 class Swap extends Component {
     constructor(props) {
         super(props);
         this.state = { 
-            pictures: [] 
+            selectedFile: null 
         };
     }
 
    
-
+    fileSelectedHandler = event => {
+        this.setState({
+            selectedFile: event.target.files[0];
+        })
+        //console.log(event.target.files[0]);
+    }
+    //set state before uploading file
+    fileUploadHandler = () => {}
     render () {
       return (
         <div>
         //need to be able to upload an image to database
-    
+        <input type="file" onChange={this.fileSelectedHandler}/>
+        <button onClick={this.fileUploadHandler}>Upload</button>
         <input/>
         //user location which is country
         //about
