17fdf0e42bf96b755b1ccd1505aaff33f8ef9ecc
YordaniBonilla <b.yordani93@gmail.com>
2019-06-05 14:58:48 -0500

    imported ImageUploader and created elements

--- client/src/Components/Swap.js
+++ client/src/Components/Swap.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ImageUploader from 'react-images-upload';
 
-function Swap() {
-    return (
+class Swap extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            pictures: [] 
+        };
+    }
+
+    onDrop(picture) {
+        this.setState({
+            pictures: this.state.pictures.concat(picture),
+        });
+    }
+
+    render () {
+      return (
+        <div>
         //need to be able to upload an image to database
+        <ImageUploader
+                withIcon={true}
+                buttonText='Choose images'
+                onChange={this.onDrop}
+                imgExtension={['.jpg', '.gif', '.png', '.gif']}
+                maxFileSize={5242880}
+            />
+        <input/>
         //user location which is country
         //about
         //twitter 
         //facebook
         <input type='text'/>
-
-        )
+        </div>
+      );
+    }  
+        
 }
 
 export default Swap;
